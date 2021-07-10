8fcba20967c9f5df61a6fe1f3516aaec515cadd1
sangsun <hiddenparts@gmail.com>
2021-07-10 18:57:56 +0900

    Initial Commit

---
+++ chaiClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chaiClone
+//
+//  Created by sangsun on 2021/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
